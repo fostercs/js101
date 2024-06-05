1ef4f68ac9506d0243bdaeffd638ef7bcf731940
Mike Hacker <mhackersu@gmail.com>
2024-06-05 12:59:42 -0500

    update index

--- index.js
+++ index.js
@@ -9,16 +9,59 @@ let arr = [3, 6, 2, 9, -1, 10]; // Left
 
 const solution = (arr) => {
     // Type your solution here
-    let res = "Left";
-    console.log(arr, res);
+
+    // console.log(arr);
+
     function filterArr(arr) {
+        // rm root node
+        arr.shift();
+        // rm non-existant node
+        let index = arr.findIndex(el => el == -1);
+        arr.splice(index, 1);
         console.log("filtered array", arr);
     }
+
+    function leftBranchSum(arr) {
+        let startingIndex = 0;
+        for (let i = startingIndex; i < arr.length; i+=2) {
+            let sum =+ arr[i];
+            return sum;
+        }
+    }
+
+    function rightBranchSum(arr) {
+        let startingIndex = 1;
+        for (let i = startingIndex; i < arr.length; i+=2) {
+            let sum =+ arr[i];
+            return sum;
+        }
+    }
+
     filterArr(arr);
 
-    return arr;
+    // console.log(arr);
+
+    let leftSum = leftBranchSum(arr);
+    let rightSum = rightBranchSum(arr);
+
+    // console.log(leftSum);
+    // console.log(rightSum);
+
+    function calcLrgstBranch(leftSum, rightSum) {
+        let res = 'null';
+        console.log(leftSum, rightSum, res);
+        if (leftSum > rightSum) {
+            let res = 'Left';
+        } else if (leftSum < rightSum) {
+            let res = 'Right';
+        } else if (leftSum = rightSum){
+            return 0;
+        }
+        return res;
+        console.log(res);
+    }
+    calcLrgstBranch(leftSum, rightSum);
+
 };
 
 solution(arr);
-
-return arr;
