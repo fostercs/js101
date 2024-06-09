d73ebd406cec80d8415223b700beb91be0224cd0
Mike Hacker <mhackersu@gmail.com>
2024-06-09 01:03:05 +0000

    passing tests

--- index.test.js
+++ index.test.js
@@ -2,9 +2,9 @@ const solution = require("./index");
 
 let leftArr = [3, 6, 2, 9, -1, 10]; // Left
 let rightArr = [1, 4, 100, 5]; // Right
-let equalArr = [1, 10, 5, 1, 12, 6] // Equal
-// let emptyArr = [] // Empty tree
-// let rootOnlyArr = [1] // Only root node
+let equalArr = [1, 10, 5, 1, 12, 6]; // Equal
+let emptyArr = []; // Empty tree
+let rootOnlyArr = [1]; // Only root node
 
 test("Left Branch Sum: ", () => {
     expect(solution(leftArr)).toBe("Left");
@@ -16,4 +16,12 @@ test("Right Branch Sum: ", () => {
 
 test("Right Branch Sum: ", () => {
     expect(solution(equalArr)).toBe("Equal");
+});
+
+test("Empty Tree: ", () => {
+    expect(solution(emptyArr)).toBe(0);
+});
+
+test("Root Only: ", () => {
+    expect(solution(rootOnlyArr)).toBe(0);
 });
